265cf613a33ccf6f1c69efe40af43e895f50b958
enrico-zd <enricozada00@gmail.com>
2022-12-15 02:30:54 +0700

    ubah app.py dan tambah templates

--- app.py
+++ app.py
@@ -1,46 +1,32 @@
+from flask import Flask, render_template, request, jsonify
 import pickle
 import numpy as np
-import streamlit as st
+import os
 
-# load save model
-model1 = pickle.load(open('predict_rain_model_logreg_fix.pkl', 'rb'))
-model2 = pickle.load(open('predict_rain_model_svm_fix.pkl', 'rb'))
+app = Flask(__name__, static_url_path='/static/css/style.css')
 
 
-# judul web
-st.title('Rain Tomorrow Predict')
+@app.route("/", methods=['GET', 'POST'])
+def rain_prediction():
+    if request.method == 'GET':
+        return render_template("index.html")
+    elif request.method == 'POST':
+        print(dict(request.form))
+        rain_features = dict(request.form).values()
+        rain_features = np.array([float(x) for x in rain_features])
+        print(rain_features)
+        model = pickle.load(open('predict_rain_model_logreg_fix.pkl', 'rb'))
+        result = model.predict([rain_features])
+        rains = {
+            '0': 'Tomorrow Will Not Rain',
+            '1': 'Tomorrow Will Rain'
+        }
+        result = rains[str(result[0])]
+        return render_template('index.html', result=result)
+    else:
+        return "Unsupported Request Method"
 
-max_temp = st.text_input('Maximum Temperature')
 
-wgs = st.text_input('Wind Gust Speed')
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
 
-hum9am = st.text_input('Humiditty 9am')
-
-hum3pm = st.text_input('Humidity 3pm')
-
-pres9am = st.text_input('Pressure 9am')
-
-pres3pm = st.text_input('Pressure 3pm')
-
-temp3pm = st.text_input('Temperature 3pm')
-
-rain_today = st.text_input('Rain Today')
-
-# code foor prediction 
-rain_predict = ''
-
-# membuat tombol prediksi 
-if st.button('Predict Rain Tomorrow with Logistic Regression') :
-    rain_predict = model1.predict([[max_temp, wgs, hum9am, hum3pm, pres9am, pres3pm, temp3pm, rain_today]])
-    if (rain_predict[0]==1):
-        rain_predict = 'Tomorrow will rain'
-    else : 
-        rain_predict = 'Tomorrow will not rain'
-elif st.button('Predict Rain Tomorrow with SVM') :
-    rain_predict = model2.predict([[max_temp, wgs, hum9am, hum3pm, pres9am, pres3pm, temp3pm, rain_today]])
-    if (rain_predict[0]==1):
-        rain_predict = 'Tomorrow will rain'
-    else : 
-        rain_predict = 'Tomorrow will not rain'
-
-st.success(rain_predict)
